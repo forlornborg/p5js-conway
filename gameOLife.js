a4978a5b6ffe574d3cddd980571ab8b22a76fa29
forlornborg <zachlefevre@gmail.com>
2017-01-04 12:22:29 -0600

    Cell system display and genCells created

--- gameOLife.js
+++ gameOLife.js
@@ -3,23 +3,14 @@ console.log("in main.js");
 
 const windowWidth = innerWidth;
 const windowHeight = innerHeight;
-var numWidth, numHeight, w, h;
+var numWidth, numHeight;
 
-class cell{
-    constructor(){
+class Cell{
+    constructor(x_, y_, width_, height_, alive_){
         //constructor will give each cell the rect() arguments and make it alive
-        this.alive = true,
-        this.height = h,
-        this.width = w;
-        this.x;
-        this.y; 
-    }
-    display(){
-        for(var i = 0; i < numHeight; i++){
-            for(var j = 0; j < numWidth; j++){
-                rect(j*this.width, i*this.height, this.width, this.height);
-            }
-        }
+        this.alive = alive_,
+        this.x = x_;
+        this.y = y_; 
     }
 }
 
@@ -29,6 +20,7 @@ class cell{
 //the cell variables will only contain their own positions and alive state
 //the cellSystem will contain the height and width
 //the "this" keyword will be different for the cell objects within the cellSystem object.
+var cellSystem = {};
 
 function setup(){
     createCanvas(windowWidth,windowHeight);
@@ -36,12 +28,33 @@ function setup(){
     numWidth = 20;
     numHeight = 20;
 
-    w = windowWidth/numWidth;
-    h = windowHeight/numHeight;
-    
+    cellSystem = {
+        w: windowWidth/numWidth,
+        h: windowHeight/numHeight,
+        cellArr: [],
+        genCells: function(){
+            for(var i = 0; i < numHeight; i++){
+                for(var j = 0; j < numWidth; j++){
+                    this.cellArr.push(new Cell(j*this.w, i*this.h, this.w, this.h, int(random(2))));
+                }
+            }
+        },
+        displayCells: function(){
+            for(var i = 0; i < this.cellArr.length; i++){
+                if(this.cellArr[i].alive){
+                    fill(0);
+                }else{
+                    fill(175);
+                }
+                rect(this.cellArr[i].x, this.cellArr[i].y, this.w, this.h);
+            }
+        },
+    }
+
+    cellSystem.genCells();
 }
 
 function draw(){
-
+    cellSystem.displayCells();
 
 }
