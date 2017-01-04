eb9bf13bc24f339112c56db83b4c350f3c612b43
forlornborg <zachlefevre@gmail.com>
2017-01-03 21:16:46 -0600

    canvas created

--- gameOLife.js
+++ gameOLife.js
@@ -1,1 +1,14 @@
 console.log("in main.js");
+
+const windowWidth = innerWidth;
+const windowHeight = innerHeight;
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    background(0);
+}
+
+function draw(){
+    
+}
